23a107b2cc590b4345301806105ce75453a75fd6
zhankeldiulyrasultop1@gmail.com <zhankeldiulyrasultop1@gmail.com>
2024-10-19 17:30:48 +0500

    first commit

---
+++ src/components/custom-avatar.tsx
@@ -0,0 +1,26 @@
+import { Avatar as AntAvatar, AvatarProps } from "antd";
+
+type Props = AvatarProps & {
+  name?: string;
+};
+
+const CustomAvatar = ({ name, style, ...rest }: Props) => {
+  return (
+    <AntAvatar
+      alt={name}
+      size="small"
+      style={{
+        backgroundColor: "#87d068",
+        display: "flex",
+        alignItems: "center",
+        border: "none",
+        ...style,
+      }}
+      {...rest}
+    >
+      {name}
+    </AntAvatar>
+  );
+};
+
+export default CustomAvatar;
